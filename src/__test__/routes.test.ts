0114d5c97c046c51d970daba7355ace127292672
Bipin Bhusal Chhetri <bhusalc.bipin@gmail.com>
2024-07-19 18:43:23 -0400

    Experiment with config, performance, testing and deployment stuffs

---
+++ src/__test__/routes.test.ts
@@ -0,0 +1,10 @@
+// integration testing with supertest demo
+import app from "../server";
+import supertest from "supertest";
+
+describe("GET /", () => {
+  it("should send back some data", async () => {
+    const res = await supertest(app).get("/");
+    expect(res.body.message).toBe("hello");
+  });
+});
--- src/server.ts
+++ src/server.ts
@@ -13,9 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  console.log("Hello from express");
   res.status(200);
-  res.json({ message: "hello user" });
+  res.json({ message: "hello" });
 });
 
 app.use("/api", protect, router);
